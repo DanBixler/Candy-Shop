3086cbd491f88da801f01379c03c103f733c19de
DanBixler <dan.bixler007@gmail.com>
2020-03-18 21:08:05 -0400

    added link for thumbnails

--- CandyShop/Models/CandyRepository.cs
+++ CandyShop/Models/CandyRepository.cs
@@ -19,12 +19,12 @@ namespace CandyShop.Models
         new Candy{CandyId = 1, Name = "Chocolate Candy", Price = 5.95M, Description = "Lorem ipsum",
             Category = _categoryRepository.GetAllCategories.ToList()[1],
             ImageUrl = "https://en.wikipedia.org/wiki/Chocolate#/media/File:Chocolate_(blue_background).jpg",
-            IsInStock = true, IsOnSale=false,ImageThumbnailUrl="https://upload.wikimedia.org/wikipedia/commons/1/13/220px-HardCandy.jpg"
+            IsInStock = true, IsOnSale=false,ImageThumbnailUrl="https://upload.wikimedia.org/wikipedia/commons/7/70/Chocolate_%28blue_background%29.jpg"
             },
         new Candy{CandyId = 1, Name = "Assorted Fruit Candy", Price = 3.95M, Description = "Lorem ipsum",
             Category = _categoryRepository.GetAllCategories.ToList()[2],
             ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Betty_Crocker_Fruit_Gushers_pieces.jpg",
-            IsInStock = true, IsOnSale=false,ImageThumbnailUrl="https://upload.wikimedia.org/wikipedia/commons/1/13/220px-HardCandy.jpg"
+            IsInStock = true, IsOnSale=false,ImageThumbnailUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Betty_Crocker_Fruit_Gushers_pieces.jpg/320px-Betty_Crocker_Fruit_Gushers_pieces.jpg"
             }
         };
         public IEnumerable<Candy> GetCandyOnSale => throw new NotImplementedException();
