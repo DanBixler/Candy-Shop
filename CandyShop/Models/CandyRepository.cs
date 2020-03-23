b85d71deb4dd06de8df71b64b9219c141cd91186
DanBixler <dan.bixler007@gmail.com>
2020-03-22 20:29:59 -0400

    Updated Candy and Category repositories to use DB instead of hard-coded info.

--- CandyShop/Models/CandyRepository.cs
+++ CandyShop/Models/CandyRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,30 +9,30 @@ namespace CandyShop.Models
     
     public class CandyRepository : ICandyRepository
     {
-        private readonly ICategoryRepository _categoryRepository = new CategoryRepository();
-        public IEnumerable<Candy> GetAllCandy => new List<Candy>
+        private readonly AppDbContext _appDbContext;
+
+        public CandyRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IEnumerable<Candy> GetAllCandy 
         { 
-        new Candy{CandyId = 1, Name = "Assorted Hard Candy", Price = 4.95M, Description = "Lorem ipsum",
-            Category = _categoryRepository.GetAllCategories.ToList()[0],
-            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/1/13/HardCandy.jpg",
-            IsInStock = true, IsOnSale=false,ImageThumbnailUrl="https://upload.wikimedia.org/wikipedia/commons/archive/1/13/20191005201658%21HardCandy.jpg"
-            },
-        new Candy{CandyId = 1, Name = "Chocolate Candy", Price = 5.95M, Description = "Lorem ipsum",
-            Category = _categoryRepository.GetAllCategories.ToList()[1],
-            ImageUrl = "https://en.wikipedia.org/wiki/Chocolate#/media/File:Chocolate_(blue_background).jpg",
-            IsInStock = true, IsOnSale=false,ImageThumbnailUrl="https://upload.wikimedia.org/wikipedia/commons/7/70/Chocolate_%28blue_background%29.jpg"
-            },
-        new Candy{CandyId = 1, Name = "Assorted Fruit Candy", Price = 3.95M, Description = "Lorem ipsum",
-            Category = _categoryRepository.GetAllCategories.ToList()[2],
-            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Betty_Crocker_Fruit_Gushers_pieces.jpg",
-            IsInStock = true, IsOnSale=false,ImageThumbnailUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Betty_Crocker_Fruit_Gushers_pieces.jpg/320px-Betty_Crocker_Fruit_Gushers_pieces.jpg"
+            get
+            {
+                return _appDbContext.Candies.Include(c => c.Category);
             }
-        };
-        public IEnumerable<Candy> GetCandyOnSale => throw new NotImplementedException();
+        }
+        public IEnumerable<Candy> GetCandyOnSale
+        {
+            get
+            {
+                return _appDbContext.Candies.Include(c => c.Category).Where(p => p.IsOnSale);
+            }
+        }
         
         public Candy GetCandyById(int candyId)
         {
-        return GetAllCandy.FirstOrDefault(c => c.CandyId == candyId);
+            return _appDbContext.Candies.FirstOrDefault(c => c.CandyId == candyId);
         }
     }
 }
