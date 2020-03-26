7808bee16501c5836c8f14832979a618d3e90f20
DanBixler <dan.bixler007@gmail.com>
2020-03-26 12:55:11 -0400

    Created method to calculate price for items in cart and added sessions to Startup.cs.

--- CandyShop/Startup.cs
+++ CandyShop/Startup.cs
@@ -30,17 +30,23 @@ namespace CandyShop
             services.AddControllersWithViews();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<ICandyRepository, CandyRepository>();
+            services.AddScoped<ShoppingCart>(sc => ShoppingCart.GetCart(sc));
+
+            services.AddHttpContextAccessor();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
-                app.UseHttpsRedirection();
-                app.UseStaticFiles();
+            {                
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();           
 
             app.UseRouting();
 
